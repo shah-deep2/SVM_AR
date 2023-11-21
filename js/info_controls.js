dbd5ce2c567e4c3e998576e1ef938bd0940ffb89
Deep Shah <deepshah11111@gmail.com>
2023-11-20 22:07:30 -0800

    Info btn updated

--- js/info_controls.js
+++ js/info_controls.js
@@ -1,25 +1,11 @@
 // Get the modal
-var modal = document.getElementById("infoModal");
+var modal = document.getElementById("info-modal");
 
 // Get the button that opens the modal
-var btn = document.querySelector(".info-btn");
+var infobtn = document.querySelector(".info-btn");
 
-// Get the element that closes the modal
-var span = document.getElementsByClassName("close-btn")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+infobtn.onclick = function() {
+  modal.style.display = modal.style.display === 'none' ? 'block' : 'none';
 }
